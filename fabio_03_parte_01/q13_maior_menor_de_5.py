bdffa23bc7ca6e10952cfc4c27012d835e2b1cc3
Lennoard Silva <lennoardrai@gmail.com>
2020-03-13 09:31:31 -0300

    Add files via upload

---
+++ fabio_03_parte_01/q13_maior_menor_de_5.py
@@ -0,0 +1,9 @@
+# 13. Leia 5 (cinco) números inteiros e escreva o maior 
+# e o menor deles. Considere que todos os valores são diferentes.
+
+num1 = int(input('Insira o número 1: '))
+num2 = int(input('Insira o número 2: '))
+num3 = int(input('Insira o número 3: '))
+num4 = int(input('Insira o número 4: '))
+num5 = int(input('Insira o número 5: '))
+
