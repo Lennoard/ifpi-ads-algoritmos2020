463e3f546f556adde3962bca572de8fdb5b2d120
Lennoard <lennoard@mobills.com.br>
2022-01-14 14:07:41 -0300

    added usd and btc

--- rebase/rebase.py
+++ rebase/rebase.py
@@ -6,14 +6,16 @@ def main():
     locale.setlocale(locale.LC_MONETARY, '')
     v = 2_000_000
     print(f'Valor da Lamborghini do Abner: {colored(locale.currency(v), "yellow")}')
+    print(f'Valor da Lamborghini do Abner em USD: {colored(locale.currency(brl_to_usd(v)), "yellow")}')
+    print(f'Valor da Lamborghini do Abner em BTC: {colored(locale.currency(brl_to_btc(v)), "yellow")}')
 
 
 def brl_to_usd(brl):  # 5,54?
-    raise NotImplementedError()
+    return brl / 5.54
 
 
 def brl_to_btc(brl):  # 233.938,52
-    raise NotImplementedError()
+    return brl / 233_938.52
 
 
 if __name__ == "__main__":
